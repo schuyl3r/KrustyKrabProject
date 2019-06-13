f46a0ff2fb39161d6e89fc2cbb999ccfd05a3885
Gabriel Tan <gabtan99@gmail.com>
2019-06-13 08:41:56 +0800

    separated nav guest and footer guest

--- assets/js/login.js
+++ assets/js/login.js
@@ -2,12 +2,12 @@ const loginButton = document.querySelector('#loginUser');
 const username = document.querySelector('#usernameInput');
 const password = document.querySelector('#passwordInput');
 
-function login(){
-    if(username.value == 'admin' && password.value == 'password'){
-        alert('Success!');
-    } else{
-        alert('Please check your username or password...');
-    }
+function login() {
+	if (username.value == 'admin' && password.value == 'password') {
+		window.location = 'index.html';
+	} else {
+		alert('Please check your username or password...');
+	}
 }
 
 loginButton.addEventListener('click', login);
