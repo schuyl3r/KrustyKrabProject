b90a74b234f9512df4c389b2320799690904ff5b
Gabriel Tan <gabtan99@gmail.com>
2019-06-25 18:45:17 +0800

    fixed stuff

--- assets/js/upload.js
+++ assets/js/upload.js
@@ -1,17 +1,17 @@
-const uploadButton = document.querySelector('#uploadFile');
-const cancelButton =document.querySelector('#cancelUpload');
-const input = document.getElementById("fileInput");
+const uploadButton = document.querySelector('#import');
+const cancelButton = document.querySelector('#cancelUpload');
+const input = document.getElementById('selectFiles');
 
-function goHome(){
-   location.href='index.html';
+function goHome() {
+	location.href = 'index.html';
 }
 
-function upload(){
-    if(input.value != ""){
-        alert("File Uploaded!");
-    } else {
-        alert("No File Selected!");
-    }
+function upload() {
+	if (input.value != '') {
+		alert('File Uploaded!');
+	} else {
+		alert('No File Selected!');
+	}
 }
 
 uploadButton.addEventListener('click', upload);
